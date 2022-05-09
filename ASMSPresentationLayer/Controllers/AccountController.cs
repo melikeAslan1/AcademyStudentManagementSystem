6fbb8cbf17c81efcd9261faf2c7561caf939d218
Melike Aslan <91019037+melikeAslan1@users.noreply.github.com>
2022-05-09 11:02:13 +0300

    Login işlemi için backend yazıldı. Şifremi unuttum işlemi için backend yazılmaya başlandı.

--- ASMSPresentationLayer/Controllers/AccountController.cs
+++ ASMSPresentationLayer/Controllers/AccountController.cs
@@ -12,6 +12,9 @@ using ASMSEntityLayer.Enums;
 using ASMSEntityLayer.ViewModels;
 using ASMSEntityLayer.ResultModels;
 using ASMSBusinessLayer.ViewModels;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using System.Text.Encodings.Web;
 
 namespace ASMSPresentationLayer.Controllers
 {
@@ -46,15 +49,15 @@ namespace ASMSPresentationLayer.Controllers
                 {
                     return View(model);
                 }
+
                 //Aynı emailden tekrar kayıt olunmasın
                 var checkUserForEmail = await _userManager.FindByEmailAsync(model.Email);
-                if(checkUserForEmail!=null)
+                if (checkUserForEmail != null)
                 {
                     ModelState.AddModelError("", "Bu email ile zaten sisteme kayıt yapılmıştır!");
                     return View(model);
                 }
-                //user ı oluşturalım.
-
+                //user'ı oluşturalım
                 AppUser newUser = new AppUser()
                 {
                     Email = model.Email,
@@ -62,20 +65,24 @@ namespace ASMSPresentationLayer.Controllers
                     Surname = model.Surname,
                     CreatedDate = DateTime.Now,
                     IsDeleted = false,
-                    BirthDate = model.BirthDate.HasValue ? model.BirthDate.Value : null,
+                    BirthDate = model.BirthDate.HasValue ?
+                    model.BirthDate.Value : null,
                     Gender = model.Gender,
                     EmailConfirmed = true,
                     UserName = model.Email
-
                 };
 
-                var result = await _userManager.CreateAsync(newUser, model.Password);
-
-                if (result.Succeeded)//eklendi
-                { 
-
+                var result = await _userManager
+                    .CreateAsync(newUser, model.Password);
+                if (result.Succeeded) //eklendi
+                {
                     //rol ataması
-                    var roleResult = await _userManager.AddToRoleAsync(newUser, ASMSRoles.Student.ToString());
+                    var roleResult = await _userManager
+                        .AddToRoleAsync(newUser, ASMSRoles.Student.ToString());
+                    if (roleResult.Succeeded == false)
+                    {
+                        //Admine gizliden bir email gönder eklesin rolü
+                    }
                     //Student eklensin
                     StudentVM newStudent = new StudentVM()
                     {
@@ -83,32 +90,149 @@ namespace ASMSPresentationLayer.Controllers
                         TCNumber = model.TCNumber
                     };
                     IResult resultStudent = _studentBusinessEngine.Add(newStudent);
-                    if(resultStudent.IsSuccess==false)
+                    if (resultStudent.IsSuccess == false)
                     {
                         //Admine gizliden bir email gönder eklesin öğrenciyi
                     }
                     //email gönderilsin
                     var emailToStudent = new EmailMessage()
                     {
-                        Subject = "ASMS Sistemine HOŞ GELDİNİZ!" + newUser.Name + " " + newUser.Surname,
+                        Subject = "ASMS Sistemine HOŞ GELDİNİZ! " +
+                        newUser.Name + " " + newUser.Surname,
                         Body = "Merhaba, Sisteme kaydınız gerçekleşmiştir...",
                         Contacts = new string[] { model.Email }
                     };
                     return RedirectToAction("Login", "Account", new { email = model.Email });
-                }
 
+                }
                 else
                 {
-                    ModelState.AddModelError("", "Beklenmedik bir sorun oldu. Üye kaydı başarısız tekrar deneyiniz"!);
+                    ModelState.AddModelError("", "Beklenmedik bir sorun oldu. Üye kaydı başarısız tekrar deneyiniz!");
                     return View(model);
                 }
+
             }
             catch (Exception)
             {
-                //loglanacak.
+
+                //loglanacak
+
                 return RedirectToAction("Error", "Home");
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult Login(string email)
+        {
+            LoginViewModel model = new LoginViewModel()
+            {
+                Email = email
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                var user = await _userManager.FindByNameAsync(model.Email);
+                // var user = _userManager.FindByEmailAsync(model.Email);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Epostanız ya da şifreniz hatalıdır! Tekrar deneyiniz!");
+                    return View();
+                }
+                //TODO: son parametre bool lockoutOnFailure ile ilgili
+                //örnek yapalım
+                var result = await _signInManager.PasswordSignInAsync
+                    (user, model.Password, model.RememberMe, false);
+
+                //TODO: son parametre bool lockoutOnFailure ile ilgili
+                //if (result.IsLockedOut)
+                //{
+                //    DateTimeOffset d = user.LockoutEnd.Value;
+                //}
+                if (!result.Succeeded)
+                {
+                    ModelState.AddModelError("", "Epostanız ya da şifreniz hatalıdır! Tekrar deneyiniz!");
+                    return View();
+                }
+                //artık hoşgeldi
+                if (_userManager.IsInRoleAsync(user, ASMSRoles.Student.ToString()).Result)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                if (_userManager.IsInRoleAsync(user, ASMSRoles.Coordinator.ToString()).Result)
+                {
+                    return RedirectToAction("Dashboard", "Admin");
+                }
+                if (_userManager.IsInRoleAsync(user, ASMSRoles.StudentAdministration.ToString()).Result)
+                {
+                    return RedirectToAction("Dashboard", "Admin");
+                }
+                return RedirectToAction("Index", "Home");
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Beklenmedik bir hata oluştu! Tekrar deneyiniz");
+                //ex loglansın
+                return View(model);
             }
         }
 
+
+        [HttpGet]
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(string email)
+        {
+            try
+            {
+                var user = await _userManager.FindByEmailAsync(email);
+                if (user == null)
+                {
+                    ViewBag.ResetPasswordSuccessMessage =
+                        "Şifre yenileme talebiniz alındı! Epostanızı kontrol ediniz!";
+                    return View();
+                }
+                var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+                var codeEncode = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
+                var callBackUrl =
+                    Url.Action("ConfirmResetPassword", "Account",
+                    new { userId = user.Id, code = codeEncode }, protocol: Request.Scheme);
+                var emailMessage = new EmailMessage()
+                {
+                    Contacts = new string[] { user.Email },
+                    Subject = "ASMS - Yeni Şifre Talebi",
+                    Body = $"Merhaba {user.Name} {user.Surname}," +
+                    $"<br/> Yeni parola belirlemek için" +
+                    $"<a href='{HtmlEncoder.Default.Encode(callBackUrl)}'> buraya </a> tıklayınız..."
+                };
+                await _emailSender.SendMessage(emailMessage);
+                ViewBag.ResetPasswordSuccessMessage =
+                       "Şifre yenileme talebiniz alındı! Epostanızı kontrol ediniz!";
+                return View();
+
+            }
+            catch (Exception ex)
+            {
+                //ex loglansın
+                ViewBag.ResetPasswordFailMessage = "Beklenmedik bir hata oluştu! Tekrar deneyiniz!";
+                return View();
+            }
+        }
     }
 }
